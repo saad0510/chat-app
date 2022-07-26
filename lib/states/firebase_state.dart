fefc3088424a4a243ea53f722fd4e8a3feedf54e
Saad <ayyansaad46@gmail.com>
2022-07-27 02:31:11 +0500

    Removed the usage of firebase emulators

--- lib/states/firebase_state.dart
+++ lib/states/firebase_state.dart
@@ -7,14 +7,14 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 final providerOfFirebaseAuth = Provider<FirebaseAuth>(
   (ref) {
-    FirebaseAuth.instance.useAuthEmulator("10.0.2.2", 9099);
+    // FirebaseAuth.instance.useAuthEmulator("10.0.2.2", 9099);
     return FirebaseAuth.instance;
   },
 );
 
 final providerOfFireStore = Provider<FirebaseFirestore>(
   (ref) {
-    FirebaseFirestore.instance.useFirestoreEmulator("10.0.2.2", 8080);
+    // FirebaseFirestore.instance.useFirestoreEmulator("10.0.2.2", 8080);
     return FirebaseFirestore.instance;
   },
 );
@@ -27,14 +27,14 @@ final providerOfGoogleAuth = Provider<GoogleSignIn>(
 
 final providerOfFireStorage = Provider<FirebaseStorage>(
   (ref) {
-    FirebaseStorage.instance.useStorageEmulator("10.0.2.2", 9199);
+    // FirebaseStorage.instance.useStorageEmulator("10.0.2.2", 9199);
     return FirebaseStorage.instance;
   },
 );
 
 final providerOfRealTimeDatabase = Provider<FirebaseDatabase>(
   (ref) {
-    FirebaseDatabase.instance.useDatabaseEmulator("10.0.2.2", 9000);
+    // FirebaseDatabase.instance.useDatabaseEmulator("10.0.2.2", 9000);
     return FirebaseDatabase.instance;
   },
 );
